c1e06d644679a3b7870f0b04113883f2bee3f8ad
Konrad Studziński <konrad966wp.pl@wp.pl>
2023-04-28 14:03:08 +0200

    Modify macierz.js

--- macierz.js
+++ macierz.js
@@ -1,9 +1,10 @@
 "use strict";
-
+let row = "";
 for (let i = 1; i <= 5; i++) {
-  let row = "";
   for (let j = 1; j <= 5; j++) {
     row += j;
   }
-  console.log(row);
+  row += "\n";
 }
+
+console.log(row);
